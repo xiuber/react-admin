0eda7f48f03dd126b53453696e425bbe35e4110a
zkboys <876213774@qq.com>
2021-02-13 20:41:54 +0800

    :art: 整理代码

--- src/pages/drag-page/component-config/data-show/Tooltip.js
+++ src/pages/drag-page/component-config/data-show/Tooltip.js
@@ -17,6 +17,15 @@ export default {
     fields: [
         {label: '显示', field: 'visible', type: 'boolean', version: '', desc: '提示信息'},
         {label: '提示信息', field: 'title', type: 'string', version: '', desc: '提示信息'},
+        {
+            label: '触发方式', field: 'trigger', type: 'radio-group', defaultValue: 'hover',
+            options: [
+                {value: 'click', label: '点击'},
+                {value: 'hover', label: '悬停'},
+                {value: 'focus', label: '聚焦'},
+            ],
+            version: '', desc: '提示信息',
+        },
         {label: '气泡框位置', field: 'placement', type: 'placement', defaultValue: 'top'},
     ],
 };
