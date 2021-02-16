f48495108d5822dad9d5e83150ee0e687aafea47
zkboys <876213774@qq.com>
2021-02-16 15:40:41 +0800

    :art: 整理代码

--- src/pages/drag-page/component-config/data-show/Collapse.Panel.js
+++ src/pages/drag-page/component-config/data-show/Collapse.Panel.js
@@ -1,5 +1,5 @@
+import {handleAfterRender} from 'src/pages/drag-page/util';
 
-import {handleAfterRender} from 'src/pages/drag-page/util'
 export default {
     isContainer: true,
     dropInTo: 'Collapse',
@@ -7,5 +7,19 @@ export default {
     hooks: {
         afterRender: handleAfterRender,
     },
-    fields: [],
+    fields: [
+        // { // 不好用
+        //     label: '是否可折叠', field: 'collapsible', type: 'radio-group', version: '4.9.0',
+        //     options: [
+        //         {value: 'header', label: '点击头部'},
+        //         {value: 'disabled', label: '禁止'},
+        //     ], desc: '是否可折叠或指定可折叠触发区域',
+        // },
+        // {label: '自定义渲染每个面板右上角的内容', field: 'extra', type: 'ReactNode', version: '', desc: '自定义渲染每个面板右上角的内容'},
+        // {label: '被隐藏时是否渲染 DOM 结构', field: 'forceRender', type: 'boolean', defaultValue: false, version: '', desc: '被隐藏时是否渲染 DOM 结构'},
+        // {label: '面板头内容', field: 'header', type: 'ReactNode', version: '', desc: '面板头内容'},
+        {label: '面板头内容', field: 'header', type: 'string', version: '', desc: '面板头内容'},
+        {label: 'key', field: 'key', type: 'string', version: '', desc: '对应 activeKey'},
+        {label: '展示箭头', field: 'showArrow', type: 'boolean', defaultValue: true, version: '', desc: '是否展示当前面板上的箭头'},
+    ],
 };
